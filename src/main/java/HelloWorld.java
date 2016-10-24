362b99c6c0071f1153e659dab8e3a29bca276ce2
aevartg <aevartg@gmail.com>
2016-10-24 10:16:35 +0000

    shitty jar prufa

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,7 +1,13 @@
 
 
 public class HelloWorld {
-    public static String  helloWorld() {
-        return "Hello World!";
-    }
+    	public static void main(String[] args){
+		HelloWorld h1 = new HelloWorld();
+		String s1 = h1.helloWorld();
+		System.out.println(s1);
 }
+	public static String  helloWorld() {
+        	return "Hello World!";
+    	}
+}
+
